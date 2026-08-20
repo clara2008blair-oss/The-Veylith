3c58dfc7234c98403c66eab376647b54d21bbb87
clara2008blair-oss <clara2008blair@gmail.com>
2026-08-21 00:52:09 +0100

    Add The Veylith mod entry point

---
+++ src/main/java/com/veylith/TheVeylith.java
@@ -0,0 +1,12 @@
+package com.veylith;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TheVeylith.MOD_ID)
+public final class TheVeylith {
+    public static final String MOD_ID = "veylith";
+
+    public TheVeylith() {
+        // The Veylith mod entry point.
+    }
+}
