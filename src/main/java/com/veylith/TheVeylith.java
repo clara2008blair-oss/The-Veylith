86e2203ec56aee278504e5c2f89708af1c29841b
clara2008blair-oss <clara2008blair@gmail.com>
2026-08-21 00:53:05 +0100

    Wire The Veylith registries

--- src/main/java/com/veylith/TheVeylith.java
+++ src/main/java/com/veylith/TheVeylith.java
@@ -1,12 +1,18 @@
 package com.veylith;
 
+import com.veylith.registry.ModEntities;
+import com.veylith.registry.ModItems;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(TheVeylith.MOD_ID)
 public final class TheVeylith {
     public static final String MOD_ID = "veylith";
 
     public TheVeylith() {
-        // The Veylith mod entry point.
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.ITEMS.register(bus);
+        ModEntities.ENTITIES.register(bus);
     }
 }
